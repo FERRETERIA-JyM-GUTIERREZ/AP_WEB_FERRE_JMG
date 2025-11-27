83bbd19f4f795fe9dd36c13e620f71a62eedbbc3
JYM GutiÃ©rrez <jymgutierrez2024@gmail.com>
2025-11-27 03:49:18 -0500

    Agregar ruta temporal para actualizar Shalon a Shalom en producciÃ³n

--- jm-ferreteria-backend/routes/api.php
+++ jm-ferreteria-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
